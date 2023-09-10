ca58c95e65b712609648f9203891bd0883baeeec
milanb0z <bozic411@gmail.com>
2023-09-10 18:03:50 +0200

    New Form Layout

--- client/src/pages/NewForm.jsx
+++ client/src/pages/NewForm.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
-const NewForm = () => {
-  return <div>NewForm</div>;
+import AppBar from "@mui/material/AppBar";
+import CssBaseline from "@mui/material/CssBaseline";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+import axios from "../axios.default";
+
+const defaultTheme = createTheme();
+
+const newForm = () => {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            Create Form
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>{/* Hero unit */}</main>
+    </ThemeProvider>
+  );
 };
 
-export default NewForm;
+export default newForm;

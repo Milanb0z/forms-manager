1fcd247dd84c75933c8ef42f7df6f030c5296672
milanb0z <bozic411@gmail.com>
2023-09-10 13:28:47 +0200

    Server Init & DB Connection

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+require("dotenv").config();
+require("./config/db");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+//Router
+
+app.get("/", (req, res) => {
+  res.send("Online");
+});
+
+app.listen(PORT, () => console.log(`SERVER_ONLINE_PORT_${PORT}/`));

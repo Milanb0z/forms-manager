484dc40580e47a484541a3893883fa0cb6d63f71
milanb0z <bozic411@gmail.com>
2023-09-10 13:41:42 +0200

    UserLogin Router

--- server/router/userRouter.js
+++ server/router/userRouter.js
@@ -22,4 +22,24 @@ router.post("/signup", async (req, res) => {
   }
 });
 
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const foundUser = await User.findOne({ email });
+    if (!foundUser) {
+      return res.status(404).send({ error: "User not Found" });
+    }
+
+    const isPassValid = await foundUser.isValidPassword(password);
+    if (!isPassValid) {
+      return res.status(401).send({ error: "Password is incorrect" });
+    }
+
+    res.send(foundUser);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error });
+  }
+});
+
 module.exports = router;

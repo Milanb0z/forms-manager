cb5ffc5f046c31f7c00b6ceba4a2b2a7a0b1fcf1
milanb0z <bozic411@gmail.com>
2023-09-20 16:27:49 +0200

    Small Fixes

--- client/src/pages/ExploreForms.jsx
+++ client/src/pages/ExploreForms.jsx
@@ -13,10 +13,9 @@ import useInput from "@hooks/useInput";
 import classes from "./ExploreForms.module.scss";
 
 const listAnimation = {
-  hidden: { opacity: 1, scale: 0 },
+  hidden: { opacity: 1 },
   visible: {
     opacity: 1,
-    scale: 1,
     transition: {
       delayChildren: 0.3,
       staggerChildren: 0.2,

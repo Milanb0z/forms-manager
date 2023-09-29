8b8a1de8d2e12782369a8ad0a8158a00a9fe2a21
milanb0z <bozic411@gmail.com>
2023-09-29 17:52:47 +0200

    TextArea Component

---
+++ client/src/@ui/TextArea.jsx
@@ -0,0 +1,25 @@
+import classes from "./TextArea.module.scss";
+
+const TextArea = ({ name, value, label, ...inputProps }) => {
+  return (
+    <div className={classes.input}>
+      {label ? (
+        <label className={classes.label} htmlFor={name}>
+          {label}
+        </label>
+      ) : null}
+
+      <textarea
+        name={name}
+        className={classes.field}
+        {...inputProps}
+        rows="4"
+        cols="50"
+      >
+        {value}
+      </textarea>
+    </div>
+  );
+};
+
+export default TextArea;

e1b2a86def605d59f858a6ca785d5600e1ab8677
milanb0z <bozic411@gmail.com>
2023-09-29 17:52:27 +0200

    File Upload Component

--- client/src/@ui/index.jsx
+++ client/src/@ui/index.jsx
@@ -2,5 +2,7 @@ import Input from "./Input";
 import Button from "./Button";
 import Card from "./Card";
 import Radio from "./Radio";
+import TextArea from "./TextArea";
+import FileDropzone from "./FileDropzone";
 
-export { Input, Button, Card, Radio };
+export { Input, Button, Card, Radio, TextArea, FileDropzone };

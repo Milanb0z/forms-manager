7bc48829c17889751e1a19beb6a3a0e758a00c43
milanb0z <bozic411@gmail.com>
2024-09-14 01:03:26 +0200

    Added: Card Loading Screen

--- client/src/@ui/Card.jsx
+++ client/src/@ui/Card.jsx
@@ -1,7 +1,17 @@
+import LoadingSpinner from "@components/LoadingSpinner/LoadingSpinner";
 import classes from "./Card.module.scss";
 
-const Card = ({ children, className }) => {
-  return <div className={`${classes.card} ${className}`}>{children}</div>;
+const Card = ({ children, className, isLoading }) => {
+  return (
+    <div className={`${classes.card} ${className}`}>
+      {isLoading && (
+        <div className={classes.loading}>
+          <LoadingSpinner />
+        </div>
+      )}
+      {children}
+    </div>
+  );
 };
 
 export default Card;

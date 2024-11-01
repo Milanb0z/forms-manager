f22153d4241d989db7fab8f66109782bd8cfc088
Milan Bozic <Bozic411@gmail.com>
2024-11-01 09:58:18 +0100

    Added: Env Url Switch

--- client/src/store/apiSlice.js
+++ client/src/store/apiSlice.js
@@ -3,7 +3,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://formr.onrender.com/api",
+    baseUrl: import.meta.env.DEV
+      ? "http://localhost:3000/api"
+      : "https://formr.onrender.com/api",
     prepareHeaders: (headers) => {
       const token = localStorage.getItem("token");
       if (token) {
